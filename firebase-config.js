f5062cf5e14db0429fc889722b0ba660298a3b9c
254n6m6rkm-lang <254n6m6rkm@privaterelay.appleid.com>
2025-12-01 00:03:25 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+// Buraya Firebase web uygulama ayarlarını yazacaksın.
+// Firebase konsolunda Project settings > General > Your apps (</>) kısmından kopyalayabilirsin.
+
+export const firebaseConfig = {
+  apiKey: "BURAYA_API_KEY",
+  authDomain: "BURAYA_AUTH_DOMAIN",
+  projectId: "BURAYA_PROJECT_ID",
+  storageBucket: "BURAYA_STORAGE_BUCKET",
+  messagingSenderId: "BURAYA_SENDER_ID",
+  appId: "BURAYA_APP_ID"
+};
